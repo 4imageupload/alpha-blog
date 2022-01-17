7cdc3faebae411b009080d3bb252d8a20c7c1c50
Tuan Nguyen <tuan.nguyen@texo.design>
2022-01-17 15:23:18 +0700

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
-  # validates :username, presence: true, length: { minimum: 6, maximum: 100 }
-  # validates :email, presence: true, length: { minimum: 10, maximum: 300 }
+  validates :username, 
+            uniqueness: { case_sensitive: false }, 
+            presence: true, 
+            length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, 
+            uniqueness: { case_sensitive: false }, 
+            presence: true, 
+            length: { maximum: 105 }, 
+            format: { with: VALID_EMAIL_REGEX }
 end
