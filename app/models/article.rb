8e7306541e5aaa3f6f2182ef557aeed87fe81cac
Tuan Nguyen <tuan.nguyen@texo.design>
2022-01-17 16:14:04 +0700

    Add one-to-many association between user and article from backend

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: { minimum: 6, maximum: 100 }
-  validates :email, presence: true, length: { maximum: 105 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 end
