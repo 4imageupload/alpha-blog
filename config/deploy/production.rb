9e22a4dab213f998cf8f6bcfb05476e80fcd0080
Tuan Nguyen <tuan.nguyen@texo.design>
2022-02-13 21:19:45 +0700

    Change IP address

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 #FIXME replace 1.2.3.4 with your IP address
-server '18.141.199.116', user: 'deploy', roles: %w{web app db}
+server '52.77.248.158', user: 'deploy', roles: %w{web app db}
 set :rails_env, 'production'
